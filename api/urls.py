faf8548798ba17ccaa994ec904cce342922401c3
Collins Omariba <collinsomariba@gmail.com>
2022-07-24 22:51:41 +0300

    Added  New api view for room joining, updated the RoomJoin and Createroom components in frontend, added css to center everything

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import GetRoom, RoomView , CreateRoomview
+from .views import GetRoom, RoomView , CreateRoomview ,JoinRoom
+
 
 urlpatterns = [
     path('room', RoomView.as_view()),
     path('create-room', CreateRoomview.as_view()),
     path('get-room', GetRoom.as_view()),
+    path('join-room', JoinRoom.as_view())
 
 ]
--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
-import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import  FormHelperText  from '@material-ui/core/FormHelperText'
-import  FormControl  from '@material-ui/core/FormControl'
+import {Button,
+    Typography,
+    Grid, 
+    FormHelperText,
+    FormControl, 
+    Radio,
+    RadioGroup,
+    FormControlLabel,
+    TextField} from '@material-ui/core'
+
 import {Link} from 'react-router-dom'
-import  Radio  from '@material-ui/core/Radio'
-import  RadioGroup  from '@material-ui/core/RadioGroup'
-import  FormControlLabel  from '@material-ui/core/FormControlLabel'
-import { TextField } from '@material-ui/core'
+
 
 
 
