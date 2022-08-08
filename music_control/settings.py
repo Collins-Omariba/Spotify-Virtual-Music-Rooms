a08c0add50595acf643634c8327feec3327d1ac6
Collins Omariba <collinsomariba@gmail.com>
2022-08-08 22:31:45 +0300

    added environment variables for the django secret key and the spotify secret client key ,client id , and redirect uri , Also added a .gitignore for the .env files

--- music_control/settings.py
+++ music_control/settings.py
@@ -12,6 +12,14 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+
+# Initialise environment variables 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4rgd925six0ve%g^&+k(&8g!hl&lz)c@4g%o@_+esb0$9dqnhy'
+SECRET_KEY = env('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -125,3 +134,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,12 @@
-CLIENT_ID = "2750167bd83e47a99bbdeffdc2d942f0"
-CLIENT_SECRET = "7d2681cea08749ce9b21b939d53abfbd"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
+
+CLIENT_ID = env('SPOTIFY_CLIENT_ID')
+CLIENT_SECRET = env('SPOTIFY_CLIENT_SECRET')
+REDIRECT_URI = env('SPOTIFY_REDIRECT_URI')
+
+# CLIENT_ID = "2750167bd83e47a99bbdeffdc2d942f0"
+# CLIENT_SECRET = "7d2681cea08749ce9b21b939d53abfbd"
+# REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
